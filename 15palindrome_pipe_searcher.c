ace96f63cfdff9d8235851d992b33555c42de613
Timofey Titovets <nefelim4ag@gmail.com>
2019-12-17 00:21:01 +0300

    Implement buffered stdin read
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- 15palindrome_pipe_searcher.c
+++ 15palindrome_pipe_searcher.c
@@ -25,8 +25,14 @@ static int is_palindrome(char *palindrome) {
         return 1;
 }
 
+size_t strdlen(char *a) {
+        char *s;
+        for (s = a; *s != '\n' && *s != '\0'; s++);
+        return s - a;
+}
+
 static char *append_and_search(char *palindrome, char *num, size_t *counter) {
-        size_t numlen = strlen(num) - 1; // -1 Ignore new line char
+        size_t numlen = strdlen(num);
 
         for (size_t i = 0; i < BUF_SIZE; i++) // Shift array by numlen
                 palindrome[i] = palindrome[i + numlen];
@@ -45,6 +51,94 @@ static char *append_and_search(char *palindrome, char *num, size_t *counter) {
         return NULL;
 }
 
+struct stdin_read_buffer {
+        FILE *stdin;
+        char buffer[64 * 1024];
+        char last_line[128];
+        size_t read_chunk;
+        size_t bytes_read;
+        size_t last_line_size;
+        char *ptr;
+        char *ptr_last_line;
+        char *ptr_end;
+};
+
+static struct stdin_read_buffer B = {
+        .read_chunk = 512,
+        .stdin = NULL
+};
+
+void fix_string(char *a) {
+        for(; *a != '\n' && *a != '\0'; a++);
+        *a = '\0';
+}
+
+char *get_stdin_line(){
+        if (B.stdin == NULL)
+                B.stdin = freopen(NULL, "rb", stdin);
+
+        if (B.ptr < B.ptr_last_line) {
+                while (*B.ptr != '\n' && *B.ptr != '\0')
+                        B.ptr++;
+
+                while (*B.ptr == '\n' && B.ptr < B.ptr_last_line) {
+                        B.ptr++;
+                }
+
+                while (*B.ptr == '\0' && B.ptr < B.ptr_last_line) {
+                        B.ptr++;
+                }
+
+                //B.ptr++;
+
+                if (B.ptr >= B.ptr_last_line) {
+                        char *t = B.last_line;
+                        if (B.stdin == NULL)
+                                return NULL;
+                        return t;
+                }
+        } else {
+                size_t read_count = sizeof(B.buffer)/B.read_chunk;
+                memset(B.buffer, '\0', sizeof(B.buffer));
+                B.bytes_read = fread(B.buffer, B.read_chunk, read_count, B.stdin);
+                B.ptr = B.buffer;
+                B.ptr_end = B.buffer + sizeof(B.buffer) - 1;
+                B.ptr_last_line = B.ptr_end;
+
+                if (B.bytes_read < 0)
+                        return NULL;
+
+                while (*B.ptr_last_line != '\n' && *B.ptr_last_line != '\0') {
+                        B.ptr_last_line--;
+                }
+
+                if (*B.ptr_last_line == '\n')
+                        *B.ptr_last_line = '\0';
+
+                B.last_line_size = B.ptr_end - B.ptr_last_line;
+                memcpy(B.last_line, B.ptr_last_line + 1, B.last_line_size);
+
+                char c;
+                do {
+                        c = fgetc(B.stdin);
+                        if (c < 0) {
+                                fclose(B.stdin);
+                                B.stdin = NULL;
+                                B.last_line[B.last_line_size] = '\0';
+                                break;
+                        }
+                        if (c == '\n')
+                                break;
+                        B.last_line[B.last_line_size] = c;
+                        B.last_line_size++;
+                } while (c != '\n' && c != '\0');
+
+                B.last_line[B.last_line_size] = '\0';
+        }
+
+        return B.ptr;
+}
+
 int main(int argc, char **argv) {
         static char palindrome[BUF_SIZE * 2]; // Extra space for insert whole number
         char *success = NULL;
@@ -54,13 +148,15 @@ int main(int argc, char **argv) {
         for (int i = 0; i < TGT_SRCH_SIZE; i++)
                 palindrome[i] = 48 + i;
 
-        static size_t buf_size = BUF_SIZE;
-        static char input_buf[BUF_SIZE];
-        static char *ptr = input_buf;
 
         begin = clock();
-        while (getline(&ptr, &buf_size, stdin) > 0 && !success) {
-                success = append_and_search(palindrome, input_buf, &palindrome_offset);
+        while (!success) {
+                char *ptr = get_stdin_line();
+                if (ptr == NULL)
+                        break;
+                //fix_string(ptr);
+                // printf("%s\n", ptr);
+                success = append_and_search(palindrome, ptr, &palindrome_offset);
         }
         end = clock();
 
@@ -69,8 +165,8 @@ int main(int argc, char **argv) {
                 printf("\n");
                 printf("Palindrome found %s at byte %lu\n", success, palindrome_offset);
         } else {
-                printf("Can't find palindrome(%i) in stdin\n", TGT_SRCH_SIZE);
+                fprintf(stderr, "Can't find palindrome(%i) in stdin\n", TGT_SRCH_SIZE);
         }
 
-        printf("Time spend: %lf sec\n", (double) (end - begin) / CLOCKS_PER_SEC);
+        fprintf(stderr, "Time spend: %lf sec\n", (double) (end - begin) / CLOCKS_PER_SEC);
 }
--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ all: 15palindrome 15palindrome_pipe_10_to_36 15palindrome_pipe_searcher 15palind
 	gcc -Wall -O3 15palindrome_pipe_10_to_36.c -o 15palindrome_pipe_10_to_36
 
 15palindrome_pipe_searcher: 15palindrome_pipe_searcher.c Makefile
-	gcc -Wall -O3 15palindrome_pipe_searcher.c -o 15palindrome_pipe_searcher
+	gcc -Wall -O2 15palindrome_pipe_searcher.c -o 15palindrome_pipe_searcher
 
 15palindrome_mmap_searcher: 15palindrome_mmap_searcher.c Makefile
 	gcc -Wall -O3 15palindrome_mmap_searcher.c -o 15palindrome_mmap_searcher
