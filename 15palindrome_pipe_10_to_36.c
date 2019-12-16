405817debfbc2e570417be992b3d766894115474
Timofey Titovets <nefelim4ag@gmail.com>
2019-12-17 01:10:42 +0300

    Use buffered stdin for 10_to_36 conversion
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- 15palindrome_pipe_10_to_36.c
+++ 15palindrome_pipe_10_to_36.c
@@ -7,17 +7,18 @@
 #include <unistd.h>
 
 #include "baseconversion.h"
+#include "stdin.h"
 
 #define BUF_SIZE 32
 
 int main(int argc, char **argv) {
-        static size_t buf_size = BUF_SIZE;
-        static char input_buf[BUF_SIZE];
         static char output_buf[BUF_SIZE];
-        static char *ptr = input_buf;
 
-        while (getline(&ptr, &buf_size, stdin) > 0) {
-                base_10_to_36(output_buf, input_buf);
+        while (1) {
+                char *ptr = get_stdin_line();
+                if (ptr == NULL)
+                        break;
+                base_10_to_36(output_buf, ptr);
                 printf("%s\n", output_buf);
         }
         return 0;
